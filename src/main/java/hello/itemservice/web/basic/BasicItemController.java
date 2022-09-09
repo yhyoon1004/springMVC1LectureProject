d32e3fe7f0e114b701804a07207f1e4f763f2bdd
yunhwan <79188190+yhyoon1004@users.noreply.github.com>
2022-09-09 19:02:13 +0900

    상품 수정 및 상품목록 thymeleaf구현

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -32,6 +33,14 @@ public class BasicItemController {
 		model.addAttribute("items", items);
 		return "basic/items";
 	}
+
+	@GetMapping("/{itemId}")
+	public String item(@PathVariable long itemId, Model model){
+		Item item = itemRepository.findById(itemId);
+		model.addAttribute("item", item);
+		return "basic/item";
+	}
+
 
 	/**
 	 * 테스트용 데이터를 추가
